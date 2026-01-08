016ff083ecfd7d5b4709068e49be267c4f23d45b
digitalofficesolution <alain.antonio932@gmail.com>
2026-01-08 13:14:19 +0800

    Fix worlds route

--- routes/worldRoutes.js
+++ routes/worldRoutes.js
@@ -1,31 +1,10 @@
 import express from "express";
-import auth from "../middleware/auth.js";
-
-import {
-  createWorld,
-  getUserWorlds,
-  getWorld,
-  updateWorld,
-  requestWorldAccess,
-  approveWorldAccess,
-  rejectWorldAccess
-} from "../controllers/worldController.js";
 
 const router = express.Router();
 
-/* CREATE */
-router.post("/", auth, createWorld);
-
-/* READ */
-router.get("/user/:userId", getUserWorlds);
-router.get("/:worldId", auth, getWorld);
-
-/* UPDATE */
-router.put("/:worldId", auth, updateWorld);
-
-/* 🔐 ACCESS CONTROL */
-router.post("/:worldId/request-access", auth, requestWorldAccess);
-router.post("/:worldId/approve/:userId", auth, approveWorldAccess);
-router.post("/:worldId/reject/:userId", auth, rejectWorldAccess);
+// TEST WORLDS ROUTE
+router.get("/", (req, res) => {
+  res.json({ message: "Worlds API is working ✅" });
+});
 
 export default router;
